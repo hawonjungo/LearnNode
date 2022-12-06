312c5b184b58c678d258d8ee2a0d50bd55b1152a
hawonjungo <hawon.jungo@gmail.com>
2022-12-06 23:46:16 +1100

    Add prettier

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,10 +1,10 @@
 class NewsController {
   //GET /news
   index(req, res) {
-    res.render("news");
+    res.render('news');
   }
   show(req, res) {
-    res.send("Hello world!");
+    res.send('Hello world!');
   }
 }
 module.exports = new NewsController();
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const NewsController = require("../app/controllers/NewsController");
+const NewsController = require('../app/controllers/NewsController');
 
-router.use("/:slug", NewsController.show);
-router.use("/", NewsController.index);
+router.use('/:slug', NewsController.show);
+router.use('/', NewsController.index);
 
 module.exports = router;
