ab79f13f1938a9b0c58144573451ccb6ea688056
hawonjungo <hawon.jungo@gmail.com>
2022-12-07 01:38:12 +1100

    MVC Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,19 @@
+const Course = require("../models/Course");
+
 class SiteController {
   //GET
   index(req, res) {
-    res.render('home');
+    Course.find({}, function (err, courses) {
+      if (!err) {
+        res.json(courses);
+      } else {
+        res.status(400).json({ err: "ERR!!" });
+      }
+    });
+    // res.render("home");
   }
   search(req, res) {
-    res.render('search');
+    res.render("search");
   }
 }
 module.exports = new SiteController();
