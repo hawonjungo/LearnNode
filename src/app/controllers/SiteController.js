f06b0b05df9d8f48740a3ae7a70fd1549bb1a4c5
hawonjungo <hawon.jungo@gmail.com>
2022-12-07 23:38:03 +1100

    CRUD - READ from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,18 @@
 const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
 class SiteController {
   //GET
-  index(req, res) {
-    Course.find({}, function (err, courses) {
-      if (!err) {
-        res.json(courses);
-      } else {
-        res.status(400).json({ err: "ERR!!" });
-      }
-    });
-    // res.render("home");
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", {
+          //courses: courses ( handlebars problem)
+          courses: multipleMongooseToObject(courses),
+        });
+      })
+      // next(err)
+      .catch(next);
   }
   search(req, res) {
     res.render("search");
