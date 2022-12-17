75be40882dac53e4ed766a974db1df9e59f4746b
hawonjungo <hawon.jungo@gmail.com>
2022-12-17 13:17:34 +1100

    change to Railway

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const meController = require("../app/controllers/meController");
+const meController = require("../app/controllers/MeController");
 
 router.get("/stored/courses", meController.storedCourses);
 router.get("/trash/courses", meController.trashCourses);
