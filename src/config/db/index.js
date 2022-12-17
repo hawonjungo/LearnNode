427ebb33f32d4a8d44944c55470245974a6c924a
hawonjungo <hawon.jungo@gmail.com>
2022-12-17 15:21:14 +1100

    mongoose connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/NodeLearning_dev", {
+    await mongoose.connect("mongodb://0.0.0.0:27017/NodeLearning_dev", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
